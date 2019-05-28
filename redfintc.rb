262e801e0de4403c9ccc43e0324969128c4aa6a0
Xtina <christinasherman206@gmail.com>
2019-05-27 20:50:17 -0700

    Update redfintc.rb

--- redfintc.rb
+++ redfintc.rb
@@ -1,8 +1,9 @@
+# required for program to run correctly
 require 'httparty'
 require 'date'
 require 'terminal-table'
 
-
+# creating a class structure, setting what attributes of the class object  is readable, and the address the request is made to.
 class FoodTrucks
   attr_reader :RESULTS_QTY, :FIELDS, :HOUR_STRING, :MINUTE_STRING, :WEEK_DAY, :CTIME , :TIME_OF_QUERY, :query
   include HTTParty
@@ -20,18 +21,19 @@ class FoodTrucks
     @query = "?$select=#{@FIELDS}&$where=#{@TIME_OF_QUERY} AND dayorder=#{@WEEK_DAY}&$order=applicant ASC&$limit=#{@RESULTS_QTY}"
 
   end
-
+# defining the response from the api and establishing the offset to limit the number of responses
   def query_response(offset = 0)
     query = "#{@query}&$offset=#{offset}"
     return self.class.get(query).parsed_response
   end
 end
-
+# setting variables for pagination
   num = 0
   page = 1
   cont_to_next_page = true
   response = FoodTrucks.new.query_response(0)
 
+# creating an array to fill the table
   def fill_table_items(data)
     foodtrucks = []
     data.each do |truck|
@@ -39,7 +41,7 @@ end
     end
     return foodtrucks
   end
-
+# build the table from the data
     def build_table(response)
       table = Terminal::Table.new do |t|
         t.title = "Food trucks"
@@ -50,7 +52,7 @@ end
       return table
 
     end
-
+# method to show more trucks
     def show_more(num)
       response = FoodTrucks.new.query_response(num)
       return response
@@ -63,7 +65,7 @@ end
     end
 
     puts build_table(response)
-
+# allows user to see all items from the list or exit.
     if page == 1
       puts "Would you like to see the full list? (Y or N): "
       user_choice = gets.chomp.downcase
@@ -83,6 +85,7 @@ end
         break
 
       end
+ # allows user to see more items from the list, see previous, or exit.
     else
         puts "NEXT (page), PREV (page) , or EXIT? "
         user_choice = gets.chomp.upcase
